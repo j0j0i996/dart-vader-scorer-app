eeab892b1596969f8bdf1fcad59607a197ccccd3
j0j0i996 <jonathan.schulte@rwth-aachen.de>
2021-04-25 02:12:47 +0200

    Making bottom tab nicer

--- navigation/BottomTabNavigator.js
+++ navigation/BottomTabNavigator.js
@@ -5,27 +5,20 @@ import { Icon } from "react-native-elements";
 import InGameScreen from "../screens/InGameScreen";
 import CalibrationScreen from "../screens/CalibrationScreen";
 import GameSelectionScreen from "../screens/GameSelectionScreen";
+import SettingsScreen from "../screens/SettingsScreen";
 import colors from "../config/colors";
 
 const BottomTab = createBottomTabNavigator();
 
 export default function BottomTabNavigator() {
   return (
-    <BottomTab.Navigator>
-      <BottomTab.Screen
-        name="Game"
-        component={GameTabNavigator}
-        options={{
-          tabBarIcon: () => (
-            <Icon
-              name="play-circle-outline"
-              type="materialicons"
-              color={colors.lightgray}
-              size={28}
-            />
-          ),
-        }}
-      />
+    <BottomTab.Navigator
+      tabBarOptions={{
+        activeTintColor: colors.secondary,
+        inactiveTintColor: colors.primary,
+        keyboardHidesTabBar: true,
+      }}
+    >
       <BottomTab.Screen
         name="Calibration"
         component={CalibrationTabNavigator}
@@ -34,7 +27,35 @@ export default function BottomTabNavigator() {
             <Icon
               name="tune"
               type="materialicons"
-              color={colors.lightgray}
+              color={colors.primary}
+              size={28}
+            />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Game"
+        component={GameTabNavigator}
+        options={{
+          tabBarIcon: () => (
+            <Icon
+              name="play-circle-outline"
+              type="materialicons"
+              color={colors.primary}
+              size={28}
+            />
+          ),
+        }}
+      />
+      <BottomTab.Screen
+        name="Settings"
+        component={SettingsTabNavigator}
+        options={{
+          tabBarIcon: () => (
+            <Icon
+              name="setting"
+              type="antdesign"
+              color={colors.primary}
               size={28}
             />
           ),
@@ -74,5 +95,19 @@ function CalibrationTabNavigator() {
         options={{ headerTitle: "Calibration" }}
       />
     </CalibrationTabStack.Navigator>
+  );
+}
+
+const SettingsTabStack = createStackNavigator();
+
+function SettingsTabNavigator() {
+  return (
+    <SettingsTabStack.Navigator>
+      <SettingsTabStack.Screen
+        name="SettingsTabScreen"
+        component={SettingsScreen}
+        options={{ headerTitle: "Settings" }}
+      />
+    </SettingsTabStack.Navigator>
   );
 }

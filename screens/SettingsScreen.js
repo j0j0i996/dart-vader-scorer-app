efa0069f6f1f49de006be50d120f92cfd34dcea6
j0j0i996 <jonathan.schulte@rwth-aachen.de>
2021-04-27 01:21:03 +0200

    Show IP and name of board in settings

--- screens/SettingsScreen.js
+++ screens/SettingsScreen.js
@@ -1,6 +1,12 @@
 import { StackScreenProps } from "@react-navigation/stack";
-import React, { useState } from "react";
-import { StyleSheet, Text, TouchableOpacity, View } from "react-native";
+import React, { useEffect, useState } from "react";
+import {
+  StyleSheet,
+  Text,
+  TouchableOpacity,
+  View,
+  ScrollView,
+} from "react-native";
 import { findServer } from "../interfaces/findBoardServer";
 import colors from "../config/colors";
 import { Typography } from "../styles";
@@ -10,19 +16,40 @@ import AsyncStorage from "@react-native-async-storage/async-storage";
 
 export default function SettingsScreen({ navigation }) {
   const [findingBoard, setFindingBoard] = useState(false);
+  const [boardIP, setBoardIP] = useState("Loading...");
+  const [boardName, setBoardName] = useState("Loading...");
+
+  useEffect(() => {
+    fetchIP();
+  });
+
+  const fetchIP = async () => {
+    const fetchedIP = await AsyncStorage.getItem("@server_address");
+    setBoardIP(fetchedIP);
+  };
+
+  const fetchName = async () => {
+    const fetchedName = await AsyncStorage.getItem("@name");
+    setBoardName(fetchedName);
+  };
 
   async function onFindBoard() {
     setFindingBoard(true);
-    var res = await findServer(3000, "192.168.0.", 40);
+    try {
+      var res = await findServer(3000, "192.168.0.", 40);
+    } catch (e) {
+      console.log(e);
+    }
 
     setFindingBoard(false);
     if (res.success) {
       alert("Found board! \nIP: " + res.address);
 
-      console.log();
-
       try {
         await AsyncStorage.setItem("@server_address", res.address);
+        setBoardIP(res.address);
+        await AsyncStorage.setItem("@name", res.name);
+        setBoardName(res.name);
       } catch (e) {
         console.log(e);
       }
@@ -33,15 +60,30 @@ export default function SettingsScreen({ navigation }) {
 
   return (
     <View style={styles.container}>
-      {findingBoard ? (
-        <Pressable style={styles.buttonInactive}>
-          <Text style={styles.small_text}>Finding board...</Text>
-        </Pressable>
-      ) : (
-        <Pressable style={styles.button} onPress={onFindBoard}>
-          <Text style={styles.small_text}>Find board in local network</Text>
-        </Pressable>
-      )}
+      <ScrollView style={styles.scrollView}>
+        <View style={styles.selectionGroup}>
+          {findingBoard ? (
+            <Pressable style={styles.buttonInactive}>
+              <Text style={styles.small_text}>Finding board...</Text>
+            </Pressable>
+          ) : (
+            <Pressable style={styles.button} onPress={onFindBoard}>
+              <Text style={styles.small_text}>Find board in local network</Text>
+            </Pressable>
+          )}
+          <View style={styles.horizontal_box}>
+            <Text style={styles.header4}>Last run: </Text>
+          </View>
+          <View style={styles.horizontal_box}>
+            <Text style={styles.text}> Board name: </Text>
+            <Text style={styles.text}> {boardName} </Text>
+          </View>
+          <View style={styles.horizontal_box}>
+            <Text style={styles.text}> Board IP: </Text>
+            <Text style={styles.text}> {boardIP} </Text>
+          </View>
+        </View>
+      </ScrollView>
     </View>
   );
 }
@@ -49,16 +91,17 @@ export default function SettingsScreen({ navigation }) {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
-    alignItems: "center",
+    flexDirection: "column",
     justifyContent: "flex-start",
-    padding: 30,
+  },
+  scrollView: {
+    backgroundColor: colors.background1,
   },
   button: {
     borderRadius: 10,
     padding: 10,
     elevation: 1,
-    margin: 0,
+    margin: 10,
     backgroundColor: colors.primary,
     alignItems: "center",
   },
@@ -66,7 +109,7 @@ const styles = StyleSheet.create({
     borderRadius: 10,
     padding: 10,
     elevation: 1,
-    margin: 0,
+    margin: 10,
     backgroundColor: colors.gray,
     alignItems: "center",
   },
@@ -85,5 +128,26 @@ const styles = StyleSheet.create({
   linkText: {
     fontSize: 14,
     color: "#2e78b7",
+  },
+  selectionGroup: {
+    borderRadius: 10,
+    margin: 15,
+    padding: 10,
+    backgroundColor: colors.background2,
+  },
+  horizontal_box: {
+    margin: 0,
+    padding: 5,
+    flexDirection: "row",
+    width: "90%",
+    justifyContent: "space-between",
+  },
+  header4: {
+    ...Typography.header4,
+    color: colors.gray,
+  },
+  text: {
+    ...Typography.text,
+    color: colors.gray,
   },
 });
